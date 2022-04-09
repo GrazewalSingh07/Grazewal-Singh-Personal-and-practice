76d8960cd61e6b7c613308e7220d86c3ee618f2b
GrazewalSingh <singhgrazewal2@gmail.com>
2022-04-09 15:38:01 +0530

    Inventory-Assigment

---
+++ UNIT5/Assignments/react-inventory/src/App.jsx
@@ -0,0 +1,13 @@
+ 
+import './App.css';
+import { Inventory } from './Components/Inventory';
+
+function App() {
+  return (
+    <div className="App">
+      <Inventory/>
+    </div>
+  );
+}
+
+export default App;
---
+++ UNIT5/Assignments/react-inventory/src/Components/Inventory.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react"
+
+
+function Inventory(){
+    const data=[
+        {name:"Books", Button:"+", Button2:"-"},
+        {name:"Pens", Button:"+", Button2:"-"},
+        {name:"Notebooks", Button:"+", Button2:"-"}
+    ]
+
+    const [items, setItems]=useState({
+        Books:0,
+        Pens:0,
+        Notebooks:0,
+    })
+    const Items= Object.values(items)
+    let total=Items.reduce((cv,ac)=>{
+        return ac+cv
+    })
+    function handleChange(n,name){
+       
+        if(n===1){
+            setItems({
+                ...items,
+                [name]:items[name]+1
+            })
+        }
+        if(n===-1){
+            if(items[name]===0){
+                return
+            }
+            setItems({
+                ...items,
+                [name]:items[name]-1
+            })
+        }
+      
+      
+    }
+return(
+    <div>
+        {data.map((el)=>(
+            <Attach name={el.name} Button={el.Button} Button2={el.Button2}/>
+    ))}
+    <h3>total: {total}</h3>
+    </div>
+    
+    
+)
+
+function Attach({name,Button,Button2}){
+    
+    return(
+         <div >
+             <p>{name}:</p>
+             <button className="circlularButton" onClick={()=>handleChange(1,name)}>{ Button}</button>
+             <button className="circlularButton" onClick={()=>handleChange(-1,name)}>{ Button2}</button> 
+             <span> {items[name]} </span>
+         </div>
+    )
+}
+
+}
+
+export{Inventory}
+
+// <div>
+// <p>Books</p>
+// <button>Add one</button>
+// <button>remove one</button>
+// </div>
+// <div>
+// <p>Pens</p>
+// <button>Add one</button>
+// <button>remove one</button>
+// </div>
+// <div>
+// <p>Notebooks</p>
+// <button>Add one</button>
+// <button>remove one</button>
+// </div>
