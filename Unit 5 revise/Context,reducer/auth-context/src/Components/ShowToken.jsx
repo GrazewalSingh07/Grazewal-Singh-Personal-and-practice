efdda096f5336cc54eea186a3dd16976a35aaca2
GrazewalSingh <singhgrazewal2@gmail.com>
2022-05-27 23:09:00 +0530

    First

--- Unit 5 revise/Context,reducer/auth-context/src/Components/ShowToken.jsx
+++ Unit 5 revise/Context,reducer/auth-context/src/Components/ShowToken.jsx
@@ -1,11 +1,18 @@
 
 import { useContext } from "react"
 import { AuthContext } from "../context/AuthContext"
+import { useNavigate } from "react-router-dom"
 export const ShowToken=()=>{
-
-    const [isAuth,setIsAuth,token]=useContext(AuthContext)
+const navigate= useNavigate()
+    const [isAuth,setIsAuth,token,setToken]=useContext(AuthContext)
     console.log(token)
+    function logout(){
+        setIsAuth(false)
+        setToken("")
+        navigate("/",{replace:true})
+    }
     return <>
     <h1>{token}</h1>
+    <button onClick={logout}>Logout</button>
     </>
 }
