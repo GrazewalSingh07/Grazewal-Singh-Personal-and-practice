4274de05c7d551348d756189772c84865285b841
GrazewalSingh <singhgrazewal2@gmail.com>
2022-07-20 12:14:50 +0530

    /

---
+++ corousel/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import CaptionCarousel from './Corousel'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+       <CaptionCarousel/>
+    </div>
+  )
+}
+
+export default App
